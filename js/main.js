fad5822b387844aa6ac0aa3a2313712e1280b159
malnakli <hassoun@outlook.com>
2014-05-12 16:09:40 -0400

    imporve map

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 
 function initialize() {
         var mapOptions = {
-          center: new google.maps.LatLng(-34.397, 150.644),
-          zoom: 2
+          center: new google.maps.LatLng(23.184084, -73.704414),
+          zoom: 2,
+          disableDefaultUI: true,
+          disableDoubleClickZoom: true,
+          draggable: false,
         };
         var map = new google.maps.Map(document.getElementById("map-canvas"),
             mapOptions);
